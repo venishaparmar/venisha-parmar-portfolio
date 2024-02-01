e51530de406fc44a8f397e704a6bdabe894d32b5
Venisha Parmar <parmarvenisha725@gmail.com>
2024-02-01 09:15:59 +0530

    commit

---
+++ js/index.js
@@ -0,0 +1,61 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+const prevButtons = document.querySelectorAll('.prev');
+const nextButtons = document.querySelectorAll('.next');
+const firstProject = document.querySelector('.first');
+const lastProject = document.querySelector('.last');
+const projects = document.querySelectorAll('.project');
+const discord = document.querySelector('#discord');
+const codepen = document.querySelector('#codepen');
+const github = document.querySelector('#github');
+
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
+// makes the next project active on button click
+nextButtons.forEach(button => {
+    const project = button.parentElement.parentElement;
+
+    button.addEventListener("click", function () {
+        project.classList.toggle('active');
+        project.classList.add('hidden');
+
+        let nextProject = project.nextElementSibling ? project.nextElementSibling : firstProject;
+        nextProject.classList.toggle('hidden');
+        nextProject.classList.add('active');
+    });
+});
+
+// makes the prev project active on button click
+prevButtons.forEach(button => {
+    const project = button.parentElement.parentElement;
+
+    button.addEventListener("click", function () {
+        project.classList.toggle('active');
+        project.classList.add('hidden');
+
+        let prevProject = project == firstProject ? lastProject : project.previousElementSibling;
+        prevProject.classList.toggle('hidden');
+        prevProject.classList.add('active');
+    });
+});
+
+discord.addEventListener("click", function () {
+    window.open("http://discordapp.com/users/1075008952292614157");
+});
+
+github.addEventListener("click", function () {
+    window.open("https://github.com/venishaparmar");
+});
+
+codepen.addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/venisha-parmar-652757222/");
+});
